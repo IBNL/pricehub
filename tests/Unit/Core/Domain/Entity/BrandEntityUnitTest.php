bc5c442cdc8bc5084b9bff20c8a5677f299fe7d3
ibnl <igorbnl22@gmail.com>
2024-12-29 19:53:32 -0300

    trocando o testAttributes pelo testCreate com dataProvider

--- tests/Unit/Core/Domain/Entity/BrandEntityUnitTest.php
+++ tests/Unit/Core/Domain/Entity/BrandEntityUnitTest.php
@@ -4,38 +4,44 @@ namespace Tests\Unit\Core\Domain\Entity;
 
 use Core\Domain\Entity\BrandEntity;
 use Core\Domain\Notification\NotificationException;
-use Core\Domain\ValueObject\ValueObjectUuid;
-use DateTime;
 use PHPUnit\Framework\TestCase;
 use PHPUnit\Framework\Attributes\DataProvider;
+use Illuminate\Support\Str;
 
 class BrandEntityUnitTest extends TestCase
 {
 
-  public function testAttributes(): void
+  #[DataProvider('dataProviderCreate')]
+  public function testCreate(string $name, ?string $logo = ''): void
   {
-
-    $uuid = ValueObjectUuid::create();
-    $createdAt = new DateTime();
-
     $brandEntity = new BrandEntity(
-      id: new ValueObjectUuid($uuid),
-      name: 'new brand',
-      logo: 'logo_url',
-      created_at: $createdAt
+      name: $name,
+      logo: $logo,
     );
 
-    $this->assertEquals($uuid, $brandEntity->id());
-    $this->assertEquals('new brand', $brandEntity->name);
-    $this->assertEquals('logo_url', $brandEntity->logo);
-    $this->assertEquals($createdAt->format('Y-m-d H:i:s'), $brandEntity->createdAt());
+    $this->assertTrue(Str::isUuid($brandEntity->id()));
+    $this->assertEquals($name, $brandEntity->name);
+    $this->assertEquals($logo, $brandEntity->logo);
+    $this->assertNotEmpty($brandEntity->createdAt());
+  }
+
+  public static function dataProviderCreate(): array
+  {
+    return [
+      'valid name and filled logo' => [
+        'name' => 'new brand',
+        'logo' => 'logo_url'
+      ],
+      'valid name and logo empty' => [
+        'name' => 'new brand',
+        'logo' => ''
+      ],
+    ];
   }
 
   #[DataProvider('dataProviderValidator')]
-  public function testValidator(
-    string $name,
-    ?string $logo = ''
-  ): void {
+  public function testValidator(string $name, ?string $logo = ''): void
+  {
     $this->expectException(NotificationException::class);
 
     new BrandEntity(
