714629d67051174a62378bb8b8190e2adc2f7537
ibnl <igorbnl22@gmail.com>
2025-01-26 19:19:21 -0300

    add DailyExtractionbEntity with validator and test

---
+++ src/Core/Domain/Entity/DailyExtractionEntity.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Core\Domain\Entity;
+
+use Core\Domain\Factory\DailyExtractionEntityValidatorFactory;
+use Core\Domain\Notification\NotificationException;
+use Core\Domain\ValueObject\ValueObjectUuid;
+use DateTime;
+
+class DailyExtractionEntity extends BaseEntity
+{
+  public function __construct(
+    protected ValueObjectUuid $extraction_id,
+    protected DateTime $reference_date,
+    protected ?bool $extraction_success = false,
+    protected ?bool $send_to_process = false,
+    protected ?string $input = '',
+    protected ?string $output = '',
+    protected ?ValueObjectUuid $id = null,
+    protected ?DateTime $created_at = null
+  ) {
+    parent::__construct();
+
+    $this->input = $this->input ?? '';
+
+    $this->output = $this->output ?? '';
+
+    $this->id = $this->id ?? ValueObjectUuid::create();
+
+    $this->created_at = $this->created_at ?? new DateTime();
+
+    
+    $this->validation();
+  }
+
+  protected function validation()
+  {
+    DailyExtractionEntityValidatorFactory::create()->validate($this);
+
+    if ($this->notification->hasErrors()) {
+      throw new NotificationException(
+        $this->notification->messages('DailyExtractionEntity')
+      );
+    }
+  }
+
+}
