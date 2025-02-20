99624ef057b9886b47cfc64f1e46c4c8203cf708
ibnl <igorbnl22@gmail.com>
2025-02-20 20:49:53 -0300

    add DailyExtractionEntity com teste de unidade

--- src/Core/Domain/Entity/DailyExtractionEntity.php
+++ src/Core/Domain/Entity/DailyExtractionEntity.php
@@ -14,22 +14,32 @@ class DailyExtractionEntity extends BaseEntity
     protected DateTime $reference_date,
     protected ?bool $extraction_success = false,
     protected ?bool $send_to_process = false,
-    protected ?string $input = '',
-    protected ?string $output = '',
+    protected ?string $input = null,
+    protected ?string $output = null,
     protected ?ValueObjectUuid $id = null,
     protected ?DateTime $created_at = null
   ) {
     parent::__construct();
 
-    $this->input = $this->input ?? '';
+    #$this->input = $this->input ?? '';
 
-    $this->output = $this->output ?? '';
+    #$this->output = $this->output ?? '';
 
     $this->id = $this->id ?? ValueObjectUuid::create();
 
     $this->created_at = $this->created_at ?? new DateTime();
 
     
+    $this->validation();
+  }
+
+  public function update(
+    bool $extraction_success,
+    string $output
+  ): void 
+  {
+    $this->extraction_success = $extraction_success;
+    $this->output = $output;
     $this->validation();
   }
 
