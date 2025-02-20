70a36e3c9c5ea18ad1f29f2ebe068549e79f867e
ibnl <igorbnl22@gmail.com>
2025-02-20 20:07:32 -0300

    add novos seeders no DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,11 @@ class DatabaseSeeder extends Seeder
   public function run(): void
   {
     $this->call(EcommerceSeeder::class);
+    $this->call(BrandSeeder::class);
     $this->call(ExtractionTypesSeeder::class);
     $this->call(CategoriesSeeder::class);    
     $this->call(SubcategoriesSeeder::class);
     $this->call(ExtractionSeeder::class);
+    $this->call(ProductSeeder::class);   
   }
 }
