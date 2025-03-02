54b6a8fcfcc85924e083672e868fd095b1f82428
ibnl <igorbnl22@gmail.com>
2025-03-02 19:59:04 -0300

    bind do AwsSqsService com a interface

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,9 +5,11 @@ namespace App\Providers;
 use App\Repositories\Eloquent\BrandEloquentRepository;
 use App\Repositories\Eloquent\DailyExtractionEloquentRepository;
 use App\Repositories\Eloquent\ExtractionEloquentRepository;
+use App\Services\Queue\AwsSqsService;
 use Core\Domain\Repository\BrandRepositoryInterface;
 use Core\Domain\Repository\DailyExtractionRepositoryInterface;
 use Core\Domain\Repository\ExtractionRepositoryInterface;
+use Core\Domain\Services\Queue\QueueInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -33,6 +35,14 @@ class AppServiceProvider extends ServiceProvider
     $this->app->singleton(
       DailyExtractionRepositoryInterface::class,
       DailyExtractionEloquentRepository::class
+    );
+
+    /**
+     * Services
+     */
+    $this->app->bind(
+      QueueInterface::class,
+      AwsSqsService::class,
     );
   }
 
